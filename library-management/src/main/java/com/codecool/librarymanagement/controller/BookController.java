503fdabacd580b3f768bc3e3dde185b702bd4e7a
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-06-24 10:13:44 +0200

    added method snippets in BookController

--- library-management/src/main/java/com/codecool/librarymanagement/controller/BookController.java
+++ library-management/src/main/java/com/codecool/librarymanagement/controller/BookController.java
@@ -1,8 +1,10 @@
 package com.codecool.librarymanagement.controller;
+
 import com.codecool.librarymanagement.dao.BookDao;
 import com.codecool.librarymanagement.model.generated.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,8 +20,39 @@ public class BookController {
     }
 
     @GetMapping("/books")
-    public List<Book> fetchBooksByMongo(){
+    public List<Book> fetchBooksByMongo() {
         return bookDao.getBookList();
     }
+
+    @GetMapping("/categories")
+    public List<String> showAllCategories() {
+        return null;
+
+    }
+
+    @GetMapping("/books/searchby/{search}")
+    public List<Book> sortBy(@PathVariable("search") String search) {
+        return null;
+
+    }
+
+    @GetMapping("/books/category/{category}/searchby/{search}")
+    public List<Book> searchInCategory(@PathVariable("category") String category, @PathVariable("search") String search) {
+        return null;
+
+    }
+
+    @GetMapping("/books/sort")
+    public List<Book> sortBy() {
+        return null;
+
+    }
+
+    @GetMapping("/books/category/{category}/sortby/{sort}")
+    public List<Book> sortInCategory(@PathVariable("category") String category, @PathVariable("sort") String search) {
+        return null;
+
+    }
+
 
 }
